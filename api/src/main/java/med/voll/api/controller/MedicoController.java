4dcf97d588415507a9c845a8bae15ff110c54a78
lzmattana <l.z.mattana@gmail.com>
2023-05-08 09:08:48 -0300

    add func lista medicos

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,14 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("medicos") // url
 @RestController // avisar o spring carregar iniciando o projeto
@@ -29,7 +29,14 @@ public class MedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
         System.out.println(dados);
-
-
     }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        // metodo para trasf medicos em dadolstmed
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList(); // medoto vem do medrep -> jparepo
+    }
+
+
+
 }
