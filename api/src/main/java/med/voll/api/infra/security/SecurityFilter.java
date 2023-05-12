afc362424cb741d19f314f93f4c385923eb7948c
lzmattana <l.z.mattana@gmail.com>
2023-05-12 11:32:27 -0300

    recuperando token

--- api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -15,7 +15,17 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
         // essa linha garante que o prox filtro seja exec
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var autorizationHeader = request.getHeader("Authorization");
+        if(autorizationHeader == null){
+            throw new RuntimeException("TOKENJWT NÃO ENVIADO NO CABEÇAO AUTHORIZATION");
+        }
+        return autorizationHeader.replace("Bearer ", "");
     }
 }
